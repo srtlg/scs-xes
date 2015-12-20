5b4b5527c6f1592b6dc8cd46d1f153bbf3cd9b7f
Mirko Scholz <mirko@alhaud>
2015-12-20 22:29:32 +0100

    bugfix: cythonize is not called with changed parameters profiling is still broken

--- setup.py
+++ setup.py
@@ -4,6 +4,21 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 
 
+def _force_rebuild(extensions):
+    '''
+    build --inplace --force does not re-cythonize the sources
+    and w/o profiling enabled, during profile the cython-module
+    may segfault
+    '''
+    for extension in extensions:
+        assert isinstance(extension, Extension)
+        for f in extension.sources:
+            if f.endswith('.pyx'):
+                cfile = f.replace('.pyx', '.c')
+                if os.path.exists(cfile):
+                    os.unlink(cfile)
+
+
 extensions = [
     Extension('scs_xes.csm.cluster',
         ['scs_xes/csm/cluster.pyx']),
@@ -14,10 +29,12 @@ if int(os.getenv('profile', '0')):
     print('profile build')
     cython_kwargs['profile'] = True
     cython_kwargs['linetrace'] = True
+    _force_rebuild(extensions)
 if int(os.getenv('unsafe', '0')):
     print('aggressive optimizations')
     cython_kwargs['wraparound'] = False
     cython_kwargs['boundscheck'] = False
+    _force_rebuild(extensions)
 
 setup(
     name='scs_xes',
